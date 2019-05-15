e10dad94bac88695c4295d8005e7f57840a4a3f2
George Joseph Kristian <jorji.gjk@gmail.com>
2019-05-15 15:44:48 +0700

    FacilitatorModel (Subclass from LearnerModel) CD 18

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -14,15 +14,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var learnerGenderLabel: UILabel!
     
     var learnerInstance : LearnerModel?
+    var facilitatorInstance : FacilitatorModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         learnerInstance = LearnerModel(nameLearner: "George", ageLearner: 21, genderLearner: "Male", imageProfileLearner: "URL")
+        facilitatorInstance = FacilitatorModel(facilName: "Zein", facilAge: 28, facilGender: "Male", facilImageProfile: "URL", facilPerk: "Technical")
         updateUI()
     }
     
     func updateUI(){
-        if let instance = learnerInstance {
+        if let instance = facilitatorInstance {
             learnerNameLabel.text = instance.name
             learnerAgeLabel.text = "\(instance.age)"
             learnerGenderLabel.text = instance.gender
@@ -30,8 +32,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func increaseAgeButtonClicked(_ sender: Any) {
-        if let instance = learnerInstance{
+        if let instance = facilitatorInstance{
             instance.increaseAge()
+            instance.coachMentee()
             updateUI()
         }
     }
