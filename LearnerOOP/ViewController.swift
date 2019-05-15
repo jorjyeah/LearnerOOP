c557f5fd5c79a88808e26741c1dd73a2788b8e1e
George Joseph Kristian <jorji.gjk@gmail.com>
2019-05-15 14:19:19 +0700

    - Passes CD 10 & CD 17 Learning Objectives

--- LearnerOOP/ViewController.swift
+++ LearnerOOP/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var learnerNameLabel: UILabel!
+    @IBOutlet weak var learnerAgeLabel: UILabel!
+    @IBOutlet weak var learnerGenderLabel: UILabel!
+    
+    var learnerInstance : LearnerModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        learnerInstance = LearnerModel(nameLearner: "George", ageLearner: 21, genderLearner: "Male", imageProfileLearner: "URL")
+        updateUI()
+    }
+    
+    func updateUI(){
+        if let instance = learnerInstance {
+            learnerNameLabel.text = instance.name
+            learnerAgeLabel.text = "\(instance.age)"
+            learnerGenderLabel.text = instance.gender
+        }
+    }
+    
+    @IBAction func increaseAgeButtonClicked(_ sender: Any) {
+        if let instance = learnerInstance{
+            instance.increaseAge()
+            updateUI()
+        }
     }
 }
